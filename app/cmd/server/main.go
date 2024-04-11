842a613c2268c9cc39924b6a1b07c89c62282a4c
117503445 <t117503445@gmail.com>
2024-04-11 05:37:41 +0000

    feat: graceful
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- app/cmd/server/main.go
+++ app/cmd/server/main.go
@@ -15,28 +15,35 @@ func main() {
 	// read version from env
 	ver := os.Getenv("VER")
 
-	log.Printf("Starting server version: %s, HOSTNAME: %s\n", ver, os.Getenv("HOSTNAME"))
-
 	if buildText, err := os.ReadFile("build.txt"); err == nil {
 		log.Printf("Build info: %s", string(buildText))
-	}else{
+	} else {
 		log.Printf("Failed to read build info: %s", err)
 	}
 
+	isGraceENV := os.Getenv("GRACE")
+	isGrace := isGraceENV == "true"
+
+	log.Printf("Starting server version: %s, HOSTNAME: %s, isGrace: %v\n", ver, os.Getenv("HOSTNAME"), isGrace)
 	server := server.NewServer(ver)
-	go func() {
+
+	if isGrace {
+		go func() {
+			server.Run()
+		}()
+
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+		log.Println("Shutting down server...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		server.Stop(ctx)
+
+		log.Println("Server exiting")
+	} else {
 		server.Run()
-	}()
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Println("Shutting down server...")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	server.Stop(ctx)
-
-	log.Println("Server exiting")
+	}
 }
